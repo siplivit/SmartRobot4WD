e016659d5ff9abd83a5969b06eb8089d9b00c6b9
siplivit <siplivit@gmail.com>
2017-07-18 01:25:17 +0300

    Start

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: vitaliy
+ *
+ * Created on July 18, 2017, 1:22 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
